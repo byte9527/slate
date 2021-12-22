ecad766751c99f696a78387667d309b5f7893c3d
FallenMax <FallenMax@gmail.com>
2021-12-22 11:39:32 +0800

    Revert "fix: insert new node in advance to avoid ime double input with editor marks (#4451)"
    This reverts commit 8e4120ae315151705152e62944737ca4f62ad446.

--- packages/slate-react/src/components/editable.tsx
+++ packages/slate-react/src/components/editable.tsx
@@ -138,7 +138,6 @@ export const Editable = (props: EditableProps) => {
   const state = useMemo(
     () => ({
       isComposing: false,
-      hasInsertPrefixInCompositon: false,
       isDraggingInternally: false,
       isUpdatingSelection: false,
       latestElement: null as DOMElement | null,
@@ -780,23 +779,6 @@ export const Editable = (props: EditableProps) => {
                 ) {
                   Editor.insertText(editor, event.data)
                 }
-
-                if (editor.selection && Range.isCollapsed(editor.selection)) {
-                  const leafPath = editor.selection.anchor.path
-                  const currentTextNode = Node.leaf(editor, leafPath)
-                  if (state.hasInsertPrefixInCompositon) {
-                    state.hasInsertPrefixInCompositon = false
-                    Editor.withoutNormalizing(editor, () => {
-                      // remove Unicode BOM prefix added in `onCompositionStart`
-                      const text = currentTextNode.text.replace(/^\uFEFF/, '')
-                      Transforms.delete(editor, {
-                        distance: currentTextNode.text.length,
-                        reverse: true,
-                      })
-                      Transforms.insertText(editor, text)
-                    })
-                  }
-                }
               }
             },
             [attributes.onCompositionEnd]
@@ -819,42 +801,9 @@ export const Editable = (props: EditableProps) => {
                 hasEditableTarget(editor, event.target) &&
                 !isEventHandled(event, attributes.onCompositionStart)
               ) {
-                const { selection, marks } = editor
-                if (selection) {
-                  if (Range.isExpanded(selection)) {
-                    Editor.deleteFragment(editor)
-                    return
-                  }
-                  const inline = Editor.above(editor, {
-                    match: n => Editor.isInline(editor, n),
-                    mode: 'highest',
-                  })
-                  if (inline) {
-                    const [, inlinePath] = inline
-                    if (Editor.isEnd(editor, selection.anchor, inlinePath)) {
-                      const point = Editor.after(editor, inlinePath)!
-                      Transforms.setSelection(editor, {
-                        anchor: point,
-                        focus: point,
-                      })
-                    }
-                  }
-                  // insert new node in advance to ensure composition text will insert
-                  // along with final input text
-                  // add Unicode BOM prefix to avoid normalize removing this node
-                  if (marks) {
-                    state.hasInsertPrefixInCompositon = true
-                    Transforms.insertNodes(
-                      editor,
-                      {
-                        text: '\uFEFF',
-                        ...marks,
-                      },
-                      {
-                        select: true,
-                      }
-                    )
-                  }
+                const { selection } = editor
+                if (selection && Range.isExpanded(selection)) {
+                  Editor.deleteFragment(editor)
                 }
               }
             },
